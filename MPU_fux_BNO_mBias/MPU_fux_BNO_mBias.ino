3b6544bbd4f5eb493aa2c9f73ecc82f322d92857
amazonenseDoCarai <joao.danilo.ar@gmail.com>
2026-03-06 15:19:43 +0000

    changed shite

--- MPU_fux_BNO_mBias/MPU_fux_BNO_mBias.ino
+++ MPU_fux_BNO_mBias/MPU_fux_BNO_mBias.ino
@@ -1077,33 +1077,33 @@ void MPU9250SelfTest(float * destination) // Should return percent deviation fro
 }
 
         
-        // fakeWire.h read and write protocols
+        // Wire.h read and write protocols
         void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
 {
-	fakeWire.beginTransmission(address);  // Initialize the Tx buffer
-	fakeWire.write(subAddress);           // Put slave register address in Tx buffer
-	fakeWire.write(data);                 // Put data in Tx buffer
-	fakeWire.endTransmission();           // Send the Tx buffer
+	Wire.beginTransmission(address);  // Initialize the Tx buffer
+	Wire.write(subAddress);           // Put slave register address in Tx buffer
+	Wire.write(data);                 // Put data in Tx buffer
+	Wire.endTransmission();           // Send the Tx buffer
 }
 
         uint8_t readByte(uint8_t address, uint8_t subAddress)
 {
 	uint8_t data; // `data` will store the register data	 
-	fakeWire.beginTransmission(address);         // Initialize the Tx buffer
-	fakeWire.write(subAddress);	                 // Put slave register address in Tx buffer
-	fakeWire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
-	fakeWire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address 
-	data = fakeWire.read();                      // Fill Rx buffer with result
+	Wire.beginTransmission(address);         // Initialize the Tx buffer
+	Wire.write(subAddress);	                 // Put slave register address in Tx buffer
+	Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+	Wire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address 
+	data = Wire.read();                      // Fill Rx buffer with result
 	return data;                             // Return data read from slave register
 }
 
         void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
 {  
-	fakeWire.beginTransmission(address);   // Initialize the Tx buffer
-	fakeWire.write(subAddress);            // Put slave register address in Tx buffer
-	fakeWire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+	Wire.beginTransmission(address);   // Initialize the Tx buffer
+	Wire.write(subAddress);            // Put slave register address in Tx buffer
+	Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
 	uint8_t i = 0;
-        fakeWire.requestFrom(address, count);  // Read bytes from slave register address 
-	while (fakeWire.available()) {
-        dest[i++] = fakeWire.read(); }         // Put read results in the Rx buffer
+        Wire.requestFrom(address, count);  // Read bytes from slave register address 
+	while (Wire.available()) {
+        dest[i++] = Wire.read(); }         // Put read results in the Rx buffer
 }
