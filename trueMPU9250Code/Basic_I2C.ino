4e24f801057cb2bd6747f498e0f972fd9e7012a5
amazonenseDoCarai <joao.danilo.ar@gmail.com>
2026-03-06 17:14:54 +0000

    changed shite

--- trueMPU9250Code/Basic_I2C.ino
+++ trueMPU9250Code/Basic_I2C.ino
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 #include "MPU9250.h"
 
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0xFF);
+MPU9250 IMU(Wire, 0x68);
 int status;
 
 void setup() {
